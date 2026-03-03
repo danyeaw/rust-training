5fb549a567e634d2a15c0d80e24c0c8ec61cefaf
Dan Yeaw <dan@yeaw.me>
2026-03-03 11:00:13 -0500

    Add sync to async exercise

--- exercises/4-multitasking/3-asynchronous-multitasking/1-sync-to-async/src/main.rs
+++ exercises/4-multitasking/3-asynchronous-multitasking/1-sync-to-async/src/main.rs
@@ -3,32 +3,25 @@
 /// - Run this binary with `cargo run`
 /// - Run a client with `cargo run --bin client` or `nc 127.0.0.1 8080`
 
-/// 1. Remove me!
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
-
 /// 2. Uncomment us
-// use tokio::net::TcpListener;
-// use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 /// 3. Uncomment me & follow the compiler (down the rabbit hole)
-// #[tokio::main]
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
     loop {
-        let (mut socket, _) = listener.accept()?;
+        let (mut socket, _) = listener.accept().await?;
 
         /// 4. Replace std::thread with tokio::task & follow the compiler (just a bit deeper)
-        std::thread::spawn(move || {
-            // tokio::task::spawn(async move {
+            tokio::task::spawn(async move {
             let mut buf = [0; 1024];
 
             // In a loop, read data from the socket and write the data back.
             loop {
-                let n = match socket.read(&mut buf) {
+                let n = match socket.read(&mut buf).await {
                     // socket closed
                     Ok(n) if n == 0 => return,
                     Ok(n) => n,
@@ -41,7 +34,7 @@ fn main() -> std::io::Result<()> {
                 println!("Got a message with {n} bytes... gonna send that right back");
 
                 // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]) {
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
                     eprintln!("failed to write to socket; err = {:?}", e);
                     return;
                 }
